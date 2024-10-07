6860e2b49a03751024b32562d130573dd880e66f
Marconidas <marquitosbcn80@gmail.com>
2024-10-07 21:06:04 +0200

    Add files via upload

---
+++ IronHack/Modulo3/W15D1/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import NabvarComponent from './components/NabvarComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+import { ref } from 'vue'
+//import { reactive, computed } from 'vue';
+
+const num = ref(0)
+const boolean = ref(true)
+
+function sumaReactiva() {
+  const valor = num.value + num.value
+  return valor
+}
+//console.log(sumaReactiva.value)
+</script>
+
+<template>
+  <div id="principal">
+    <h1>Week15D1</h1>
+    <NabvarComponent></NabvarComponent>
+    <p>Resultado de 2 + 2: {{ 2 + 2 }}</p>
+    <br />
+    <input name="num" id="num" v-model="num" type="number" />
+    <p>{{ sumaReactiva() }}</p>
+    <br />
+    <p v-if="boolean">Ahora me ves</p>
+    <FooterComponent></FooterComponent>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: aqua;
+}
+</style>
---
+++ IronHack/Modulo3/W15D1/src/components/NabvarComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+//import { ref } from 'vue'
+</script>
+
+<template>
+  <h1>NavBar Component</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: brown;
+  background-color: blueviolet;
+}
+</style>
